e317be3f67fb3dea6731070a44d6ff0e29b80e74
ibrahim wael <ibrahimwael809@gmail.com>
2025-02-13 00:30:21 +0200

    add navbar

---
+++ JavaScript.js
@@ -0,0 +1,16 @@
+ const Btn = document.getElementById("mobileBtn")
+ const Menu =document.getElementById("menuMobile")
+
+//  Btn.onclick =function(){
+//     if(Menu.style.transform == "translateX(170px)"){
+//         Btn.style.transform ="translateX(-140px)"
+//         Menu.style.transform = "translateX(0)";
+//     }
+//     else{
+//         Menu.style.transform = "translateX(170px)";
+//          Btn.style.transform ="translateX(0px)"
+//     }
+//  }
+
+
+ 
